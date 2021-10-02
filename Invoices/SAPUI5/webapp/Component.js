86f6f51733fcc96021dbc7c7c1a0c9417fef1d53
rcdesarrollo <rcdesarrollo@gmail.com>
2021-10-02 14:22:58 +0000

    Interfaz de Usuario

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -1,17 +1,19 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "logaligroup/SAPUI5/model/Models",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 ],
     /**
      * @param {typeof sap.ui.core.UIComponent} UIComponent
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      */
-    function (UIComponent, Models, ResourceModel) {
+    function (UIComponent, Models, ResourceModel, HelloDialog) {
 
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
 
             metadata: {
-                manifest : "json"
+                manifest: "json"
             },
 
             init: function () {
@@ -24,7 +26,19 @@ sap.ui.define([
                 // Set i18n model on the view
                 var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n");
+
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            },
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+            openHelloDialog: function () {
+                this._helloDialog.open();
             }
+
+
+
         });
 
     });
