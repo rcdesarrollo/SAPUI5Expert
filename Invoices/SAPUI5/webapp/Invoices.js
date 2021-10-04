a901d1cbcb3a685d453117a82e912b76e0ee29e9
rcdesarrollo <rcdesarrollo@gmail.com>
2021-10-04 19:28:25 +0000

    Binding y Formateadores

---
+++ Invoices/SAPUI5/webapp/Invoices.js
@@ -0,0 +1,28 @@
+{
+    "Invoices": [
+        {
+            "ProductName" : "HDD-Samsung 250bGB",
+            "Quantity" : 21,
+            "ExtendedPrice": 87.2000,
+            "ShipperName": "Fun Inc.",
+            "ShippedDate":"2015-04-01T00:00:00",
+            "Status" :"A"
+        },
+        {
+            "ProductName" : "Display Dell UH850 4K 27",
+            "Quantity" : 4,
+            "ExtendedPrice": 9.99999,
+            "ShipperName": "ACME.",
+            "ShippedDate":"2015-02-18T00:00:00",
+            "Status" :"B"
+        },
+        {
+            "ProductName" : "RAM PX DDR4 PC4-24000 16GB",
+            "Quantity" : 3,
+            "ExtendedPrice": 6.85000,
+            "ShipperName": "ACME.",
+            "ShippedDate":"2015-03-02T00:00:00",
+            "Status" :"B"
+        }                        
+    ]
+}
