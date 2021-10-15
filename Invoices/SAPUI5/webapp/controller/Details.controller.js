cb5f7f63f60acd862a681c4020c91f8a9c7106c4
rcdesarrollo <rcdesarrollo@gmail.com>
2021-10-15 20:54:33 +0000

    Inicial

--- Invoices/SAPUI5/webapp/controller/Details.controller.js
+++ Invoices/SAPUI5/webapp/controller/Details.controller.js
@@ -16,6 +16,7 @@ sap.ui.define([
         return Controller.extend("logaligroup.SAPUI5.controller.Details", {
 
             _onObjectMatch: function(oEvent){
+                this.byId("rating").reset();
                 this.getView().bindElement({
                     path : "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                     model : "northwind"
@@ -37,6 +38,13 @@ sap.ui.define([
                     const oRouter = UIComponent.getRouterFor(this);
                     oRouter.navTo("RouteApp",{},true);
                 }
+            },
+
+             onRatingChange: function(oEvent){
+                const fValue = oEvent.getParameter("value");
+                const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+                sap.m.MessageToast.show(oResourceBundle.getText("ratingConfirmation",[fValue]));    
             }
 
 
