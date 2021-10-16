1eb8a482841094f0ca104a0dd3b4c5908fd9a8fc
rcdesarrollo <rcdesarrollo@gmail.com>
2021-10-16 11:02:15 +0000

    Adaptaciones

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -2,13 +2,15 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "logaligroup/SAPUI5/model/Models",
     "sap/ui/model/resource/ResourceModel",
-    "./controller/HelloDialog"
+    "./controller/HelloDialog",
+    "sap/ui/Device"
 ],
     /**
      * @param {typeof sap.ui.core.UIComponent} UIComponent
      * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+     * @param {typeof sap.ui.Device} Device
      */
-    function (UIComponent, Models, ResourceModel, HelloDialog) {
+    function (UIComponent, Models, ResourceModel, HelloDialog, Device) {
 
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
 
@@ -27,6 +29,9 @@ sap.ui.define([
                 // var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
                 // this.setModel(i18nModel, "i18n");
 
+                // set the device model
+                this.setModel(Models.createDeviceModel(),"device");
+
                 this._helloDialog = new HelloDialog(this.getRootControl());
 
                 // create the views based on the url/hash
@@ -38,9 +43,17 @@ sap.ui.define([
             },
             openHelloDialog: function () {
                 this._helloDialog.open();
+            },
+
+            getContentDensityClass: function(){
+                if(!Device.support.touch){
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                }else{
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                }
+                
+                return this._sContentDensityClass;
             }
-
-
 
         });
 
--- Invoices/SAPUI5/webapp/model/Models.js
+++ Invoices/SAPUI5/webapp/model/Models.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device"
 ],
     /**
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.Device } Device
      */
-    function (JSONModel) {
+    function (JSONModel, Device) {
         "use strict";
         return {
             createRecipient: function () {
@@ -15,6 +17,12 @@ sap.ui.define([
                 }
 
                 return new JSONModel(oData);
+            },
+
+            createDeviceModel: function(){
+                var oModel = new JSONModel(Device);
+                oModel.setDefaultBindingMode("OneWay");
+                return oModel;
             }
 
         }
